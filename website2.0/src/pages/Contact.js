e3de51c96212a9100727669bec6ae16e8e839027
Matthew Serdukoff <matthewserdukoff@matthews-mbp.mynetworksettings.com>
2024-06-16 19:54:09 -0400

    react project

---
+++ website2.0/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="content">
+      <h2>Contact</h2>
+      <p>Get in touch with me.</p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ website2.0/src/pages/Photography.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Photography = () => {
+  return (
+    <div className="content">
+      <h2>Photography</h2>
+      <p>My photography works.</p>
+    </div>
+  );
+};
+
+export default Photography;
